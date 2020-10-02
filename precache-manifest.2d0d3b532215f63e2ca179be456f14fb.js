f55d13e80bd828bb9e6cffcba041f2ee248d6a20
Kevin Prakarsa <kevin.prakarsa@yahoo.com>
2020-10-02 18:58:12 +0800

    Updates

---
+++ precache-manifest.2d0d3b532215f63e2ca179be456f14fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd549038e102e9770898b4ba5d5faae",
+    "url": "/resume-kevin/index.html"
+  },
+  {
+    "revision": "0e32401dc96087dd51bd",
+    "url": "/resume-kevin/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c252c19e0e1b5a915ca",
+    "url": "/resume-kevin/static/js/2.72ce34e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/resume-kevin/static/js/2.72ce34e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e32401dc96087dd51bd",
+    "url": "/resume-kevin/static/js/main.0904af5a.chunk.js"
+  },
+  {
+    "revision": "5776a34b44b5e15928fd",
+    "url": "/resume-kevin/static/js/runtime-main.cd7ee406.js"
+  },
+  {
+    "revision": "630a1ead6491ba8982ada574a387c975",
+    "url": "/resume-kevin/static/media/Kevin.630a1ead.jpg"
+  }
+]);
