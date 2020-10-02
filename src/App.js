4d957c3012cd8005365bfc058b8f2a2fab47ca91
Kevin Prakarsa <kevin.prakarsa@yahoo.com>
2020-10-02 18:58:59 +0800

    feat: deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-//import Index from './pages/Index';
-import AboutMe from './pages/AboutMe';
+import Index from './pages/Index';
+//import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
     <div className="App">
-      {/* <Index /> */}
-      <AboutMe/>
+      <Index />
+      {/* <AboutMe/> */}
     </div>
   );
 }
